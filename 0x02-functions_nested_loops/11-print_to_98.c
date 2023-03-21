5c56b5dc7feb4bfaaf3c2a90a12a8b7154056492
heraw254 <allandavis254@gmail.com>
2023-03-21 10:02:17 -0700

    Battery status

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,7 @@
   * print_to_98 - function to print natural numbers
   *
   * @n: function parameter
+  *
   * Return: always 0
   */
 
@@ -13,29 +14,35 @@ void print_to_98(int n)
 	int i;
 
 	if (n >= 0 && n <= 98)
-	{
-		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			for (i = n; i <= 98; i++)
+			{
+				printf("%d", i);
+				if (i != 98)
+					printf(", ");
+			}
+			printf("\n");
 		}
-		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i = n; i <= 98; i++)
+		else if (n < 0)
 		{
-			printf("%d, ", i);
+			for (i = n; i <= 98; i++)
+			{
+				printf("%d", i);
+				if (i != 98)
+					printf(", ");
+			}
+			printf("\n");
 		}
-		printf("\n");
-	}
-	else if (n > 98)
-	{
-		for (i = n; i >= 98; i--)
+		else if (n > 98)
 		{
-			printf("%d, ", i);
+			for (i = n; i >= 98; i--)
+			{
+				printf("%d", i);
+				if (i != 98)
+					printf(", ");
+			}
+			printf("\n");
 		}
-		printf("\n");
-	}
-	else if (n == 98)
-		printf("%d, ", i);
+		else if (n == 98)
+			printf("%d", n);
 }

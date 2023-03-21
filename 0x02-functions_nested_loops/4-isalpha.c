5d30a31ca858ef48b16f20fce754c00df999884b
heraw254 <allandavis254@gmail.com>
2023-03-21 04:04:26 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _isalpha - Shows 1 if input is a
+  * letter Another cases, shows 0
+  *
+  *@c: Rhe character in ASCII code
+  *Return: 1 for letters. 0 for the rest
+  */
+
+int _isalpha(int c)
+{
+	((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n')
+}

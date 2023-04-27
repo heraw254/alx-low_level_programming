bdb7f691f36d39d84bc79d94b759ee05b057ffa0
heraw254 <allandavis254@gmail.com>
2023-04-27 07:32:19 -0700

    TOT

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
   */
 void __attribute__ ((constructor)) first_main()
 {
-	printf("You are beat! and yet, you must allow");
+	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }

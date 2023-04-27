c123139d516ea84e6119359a619054441500c11b
heraw254 <allandavis254@gmail.com>
2023-04-27 07:30:20 -0700

    ToT

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
   * first_main - prints a sentence before main
   * execution
   */
-void __attribute__ ((constructor)) first_main(void)
+void __attribute__ ((constructor)) first_main()
 {
 	printf("You are beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
